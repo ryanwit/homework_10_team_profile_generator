2d9ff8a28a48fb01f382c4783c5d009150ef5e71
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-10-20 10:36:54 -0600

    Update lib constructor files

--- Develop/app.js
+++ Develop/app.js
@@ -4,12 +4,121 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const team = [];
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output"); //! use with build team function
+const outputPath = path.join(OUTPUT_DIR, "team.html"); //! same as above
 
-const render = require("./lib/htmlRenderer");
+const render = require("./lib/htmlRenderer"); //! SAME
 
+/* -------------------------------------------------------------------------- */
+/*                               INITIAL-PROMPT                               */
+/* -------------------------------------------------------------------------- */
+
+
+function createMember() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "employeeSelection",
+            message: "What type of employee do you want to add",
+            choices: [
+                "Engineer",
+                "Intern",
+                "None"
+                    ]
+        },
+
+    ]).then(response => {
+        switch(response.employeeSelection) {
+            case "Engineer": 
+            generateEngineer() //TODO: Build Engineer function similar to genMgr
+            break;
+            case "Intern":
+            generateIntern() // TODO: Build INtern function see below
+            break;
+            default: //! Call function to generate HTML (buildTeam example)
+        }
+    })
+}   
+/* --------------------------------- Manager -------------------------------- */
+function generateManager() {
+    inquirer.prompt([
+        {
+        type: "input",
+        message: "What is the name of the project manager?",
+        name: "managerName"
+        },
+        {
+        type: "input",
+        message: "What office number are they in?",
+        name: "managerOffice"
+        },
+        {
+        type: "input",
+        message: "What is their email address?",
+        name: "managerEmail"
+        }, // add question about mgr id - add to new mgr const below
+        
+    ]).then((response) => {
+        const manager = new Manager(response.managerName, response.managerOffice, response.managerEmail)
+        team.push(manager)
+        createMember()
+    })
+}
+    
+
+/* -------------------------------- Engineer -------------------------------- */
+
+    else if (response.employeeSelection === Engineer) {
+        inquirer.prompt([
+            {
+            type: "input",
+            message: "What is the name of the engineer?",
+            name: "engineer-name"
+            },
+            {
+            type: "input",
+            message: "What is their GitHub username?",
+            name: "engineer-github"
+            },
+            {
+            type: "input",
+            message: "What is their email address?",
+            name: "engineer-address"
+            },
+        ])
+    } 
+
+/* --------------------------------- INTERN --------------------------------- */
+
+    else if (response.employeeSelection === Intern) {
+        inquirer.prompt([
+            {
+            type: "input",
+            message: "What is the name of the intern?",
+            name: "intern-name"
+            },
+            {
+            type: "input",
+            message: "What school do they attend?",
+            name: "intern-github"
+            },
+            {
+            type: "input",
+            message: "What is their email address?",
+            name: "intern-address"
+            },
+        ])
+
+function makeHTML() { //!bUILD TEAM
+    const html = render(employeeSelection)
+    writeFileSync("filename.html", )
+    if (err {
+        console.log(err)
+    })
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
